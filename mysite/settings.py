9781d1997d7ef4fe6c4c17e590eb6cf268731045
Elena <9396534@gmail.com>
2016-11-26 17:40:13 +0200

    'fix'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ob@l4^&sg%vfo4dydsqj@+zc@5o+!1-cmr#@+99#gu(cf=0nw5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
